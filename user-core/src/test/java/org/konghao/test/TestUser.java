747c6505f8f1498e6fcfbd11cd2483ecfeaef634
accplxm@qq.com <accplxm@qq.com>
2016-04-08 11:04:47 +0800

    maven pro

---
+++ user-core/src/test/java/org/konghao/test/TestUser.java
@@ -0,0 +1,40 @@
+package org.konghao.test;
+
+import org.hibernate.Session;
+import org.junit.Assert;
+import org.junit.Test;
+import org.konghao.util.HibernateUtil;
+import org.konghao.vo.User;
+
+
+public class TestUser
+{
+
+    @Test
+    public void testAdd()
+    {
+        Session session = HibernateUtil.openSession();
+        session.beginTransaction();
+        User u = new User();
+        u.setEmail("accplxm@qq.com");
+        u.setNickname("nicknanme");
+        u.setPassword("123");
+        u.setUsername("admin");
+        session.save(u);
+        Assert.assertTrue(u.getId() > 0);
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void testLoad()
+    {
+        Session session = HibernateUtil.openSession();
+        session.beginTransaction();
+
+        User u = (User)session.load(User.class, 1);
+
+        Assert.assertEquals(1, u.getId());
+
+        session.getTransaction().commit();
+    }
+}
