858f5a8e9ef24843ee24efc37016f42652141b0b
luxiumin <accplxm@qq.com>
2016-06-05 22:54:57 +0800

    aa
    bb

--- user-core/src/test/java/org/konghao/test/TestUser.java
+++ user-core/src/test/java/org/konghao/test/TestUser.java
@@ -6,35 +6,31 @@ import org.junit.Test;
 import org.konghao.util.HibernateUtil;
 import org.konghao.vo.User;
 
+public class TestUser {
 
-public class TestUser
-{
+	@Test
+	public void testAdd() {
+		Session session = HibernateUtil.openSession();
+		session.beginTransaction();
+		User u = new User();
+		u.setEmail("accplxm@qq.com");
+		u.setNickname("nicknanme");
+		u.setPassword("123444");
+		u.setUsername("admin");
+		session.save(u);
+		Assert.assertTrue(u.getId() > 0);
+		session.getTransaction().commit();
+	}
 
-    @Test
-    public void testAdd()
-    {
-        Session session = HibernateUtil.openSession();
-        session.beginTransaction();
-        User u = new User();
-        u.setEmail("accplxm@qq.com");
-        u.setNickname("nicknanme");
-        u.setPassword("123");
-        u.setUsername("admin");
-        session.save(u);
-        Assert.assertTrue(u.getId() > 0);
-        session.getTransaction().commit();
-    }
+	@Test
+	public void testLoad() {
+		Session session = HibernateUtil.openSession();
+		session.beginTransaction();
 
-    @Test
-    public void testLoad()
-    {
-        Session session = HibernateUtil.openSession();
-        session.beginTransaction();
+		User u = (User) session.load(User.class, 1);
 
-        User u = (User)session.load(User.class, 1);
+		Assert.assertEquals(1, u.getId());
 
-        Assert.assertEquals(1, u.getId());
-
-        session.getTransaction().commit();
-    }
+		session.getTransaction().commit();
+	}
 }
